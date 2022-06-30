838c3e03c69d8a6e07fc9472740c1b0ae4366669
Nguyen Ngoc Le <nguyenngocle2408@gmail.com>
2022-06-30 17:23:45 +0700

    .

--- 02.Calculator/MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
+++ 02.Calculator/MyCalculator/app/src/main/java/com/example/mycalculator/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
-import kotlin.math.abs
+import java.lang.ArithmeticException
 
 class MainActivity : AppCompatActivity() {
 
@@ -30,15 +30,114 @@ class MainActivity : AppCompatActivity() {
         lastDot = false
     }
 
-    fun onClear(view: View) {
+    fun onClear() {
         tvInput?.text = ""
+        lastNumeric = false
+        lastDot = false
     }
 
-    fun onDecimalPoint(view: View) {
-        if(lastNumeric&& !lastDot) {
+    fun onDecimalPoint() {
+        if (lastNumeric && !lastDot) {
             tvInput?.append(".")
             lastNumeric = false
             lastDot = true
+        }
+    }
+
+    fun onEqual(view: View) {
+        if (lastNumeric) {
+            var tvValue = tvInput?.text.toString()
+            var prefix = ""
+            try {
+                if (tvValue.startsWith("-")) {
+                    prefix = "-"
+                    tvValue = tvValue.substring(1)
+                }
+                when {
+                    tvValue.contains("+") -> {
+                        val splitedValue = tvValue.split("+")
+
+                        var one = splitedValue[0] // Value One
+                        val two = splitedValue[1] // Value Two
+
+                        if (prefix.isNotEmpty()) { // If the prefix is not empty then we will append it with first value i.e one.
+                            one = prefix + one
+                        }
+
+                        tvInput?.text =
+                            removeZeroAfterDot((one.toDouble() + two.toDouble()).toString())
+                    }
+                    //
+                    tvValue.contains("-") -> {
+                        val splitedValue = tvValue.split("-")
+                        var one = splitedValue[0]
+                        var two = splitedValue[1]
+                        if (prefix.isNotEmpty()) {
+                            one = prefix + one
+                        }
+                        tvInput?.text =
+                            removeZeroAfterDot((one.toDouble() - two.toDouble()).toString())
+                    }
+                    //
+                    tvValue.contains("*") -> {
+                        val splitedValue = tvValue.split("*")
+                        var one = splitedValue[0]
+                        var two = splitedValue[1]
+                        if(prefix.isNotEmpty()) {
+                            one = prefix + one
+                        }
+                        tvInput?.text = removeZeroAfterDot((one.toDouble() * two.toDouble()).toString())
+                    }
+                    //
+                    tvValue.contains("/") -> {
+                        val splitedValue = tvValue.split("/")
+
+                        var one = splitedValue[0] // Value One
+                        val two = splitedValue[1] // Value Two
+
+                        if (prefix.isNotEmpty()) { // If the prefix is not empty then we will append it with first value i.e one.
+                            one = prefix + one
+                        }
+
+                        tvInput?.text = removeZeroAfterDot((one.toDouble() / two.toDouble()).toString())
+                    }
+                }
+            } catch (e: ArithmeticException) {
+                e.printStackTrace()
+            }
+        }
+    }
+
+    /**
+     * Remove the zero after decimal point
+     */
+    private fun removeZeroAfterDot(result: String): String {
+
+        var value = result
+
+        if (result.contains(".0")) {
+            value = result.substring(0, result.length - 2)
+        }
+
+        return value
+    }
+
+    fun onOperator(view: View) {
+        tvInput?.text.let {
+            if (lastNumeric && !isOperatorAdded(it.toString())) {
+                tvInput?.append((view as Button).text)
+                lastNumeric = false // Update the flag
+                lastDot = false // Update the flag
+            }
+        }
+    }
+
+    private fun isOperatorAdded(value: String): Boolean {
+        return if (value.startsWith("-")) {
+            false
+        } else {
+            value.contains("/") || value.contains("*") ||
+                    value.contains("+") || value.contains("-")
         }
     }
 }
